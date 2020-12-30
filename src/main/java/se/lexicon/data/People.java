9f47d3f2c9f0faf0122a3db2981ec36a9d54a2fc
Sebastian Bocaciu <75611075+TheAppleJuice@users.noreply.github.com>
2020-12-30 14:34:53 +0100

    Added methods to the data package classes. Not finalized.

--- src/main/java/se/lexicon/data/People.java
+++ src/main/java/se/lexicon/data/People.java
@@ -1,4 +1,20 @@
 package se.lexicon.data;
+import se.lexicon.model.Person;
 
 public class People {
+    private static Person[] peopleArray= new Person[0];
+
+    public int size() {
+        return peopleArray.length;
+    }
+    public Person[] findAll(){
+        // witch of the return types below is valid for the argument?
+        //return new Person[0];
+        //return peopleArray;
+    }
+
+    public Person findById(int PERSONID) {
+        //return ???? ;
+    }
+
 }
--- src/main/java/se/lexicon/data/TodoItems.java
+++ src/main/java/se/lexicon/data/TodoItems.java
@@ -1,4 +1,20 @@
 package se.lexicon.data;
 
+import se.lexicon.model.Todo;
+
 public class TodoItems {
+    private static Todo[] todoArray= new Todo[0];
+
+    public int size() {
+        return todoArray.length;
+    }
+    public Todo[] findAll(){
+        // witch of the return types below is valid for the argument?
+        //return new Todo[0];
+        //return todoArray;
+    }
+
+    public Todo findById(int TODOID) {
+        //return ????;
+    }
 }
